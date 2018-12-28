dbe74fc41aa7e3f294b138a7f494ed81548498a4
soham009 <sohamthaker17@gmail.com>
2018-12-28 06:48:05 +0000

    Done

--- q02_top_names/build.py
+++ q02_top_names/build.py
@@ -1,3 +1,4 @@
+# %load q02_top_names/build.py
 import pandas as pd
 from collections import Counter
 from greyatomlib.babies_names_project.q01_create_dict.build import q01_create_dict
@@ -6,6 +7,14 @@ from greyatomlib.babies_names_project.q01_create_dict.build import q01_create_di
 path = 'data/babies_name.csv'
 data = pd.read_csv(path,names=['Name', 'Gender', 'Count', 'Year'])
 
-def q02_top_names():
-    "write your solution here"
+def q02_top_names(data):
+    'write your solution here'
+    dict1 = q01_create_dict(data)
+    dict2 = Counter(dict1).most_common(25)
+    return dict2, dict1
+
+# q02_top_names(data)
+
+
+
 
